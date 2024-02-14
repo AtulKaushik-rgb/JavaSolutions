609c56fa61ef24b0077acaf70b21b110a79122cf
Atul Kaushiik <atulkaushiik@Atuls-MacBook-Pro-2.local>
2024-02-14 08:47:26 +0530

    Revert "Initial commit"
    This reverts commit 78f203b51538c80483d2ebb82d1d0821481c1f7d.

--- src/main/java/org/example/intro/HelloWorldPrinter.java
+++
@@ -1,8 +0,0 @@
-package org.example.intro;
-
-public class HelloWorldPrinter implements Runnable{
-    @Override
-    public void run() {
-        System.out.println("Running helloWorldPrinter in thread "+Thread.currentThread().getName());
-    }
-}
--- src/main/java/org/example/synchronization/Count.java
+++
@@ -1,9 +0,0 @@
-package org.example.synchronization;
-
-public class Count {
-    int value;
-
-    public Count(int value) {
-        this.value = value;
-    }
-}
